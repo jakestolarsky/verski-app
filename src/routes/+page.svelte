1993b543eb6cf3197a2f24801b1290b549cc8d75
jakestolarsky <stolarczyk.jk@gmail.com>
2026-08-05 19:42:02 +0200

    create first draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<button type="submit">Subscribe</button>
+<svelte:head>
+    <title> Verski - Bible lookup done right</title>
+    	<meta
+		name="description"
+		content="Fast, offline-first Bible passage lookup."
+	/>
+</svelte:head>
+
+<main class="container">
+    <header>
+        <p>Verski</p>
+        <h1>Bible lookup done right</h1>
+    </header>
+
+    <form method="get">
+        <label for="reference">Bible reference</label>
+        <input
+            id="reference"
+            name="reference"
+            type="search"
+            placeholder="John 3:16"
+            autocomplete="off"
+        />
+        <button type="submit">Lookup</button>
+    </form>
+
+    <section aria-label="passage-heading" aria-live="polite">
+        <h2 id="passage-heading">Passage</h2>
+        <p>Enter a Bible reference to begin</p>
+    </section>
+</main>
