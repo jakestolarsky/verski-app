0fef0ba0d4cbf6f7d4caac9a399eb4c8549f1123
jakestolarsky <stolarczyk.jk@gmail.com>
2026-08-07 17:33:30 +0200

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,10 @@ export default defineConfig({
 						provider: playwright(),
 						instances: [{ browser: 'chromium', headless: true }]
 					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					include: [
+						'src/**/*.svelte.{test,spec}.{js,ts}', 
+						'src/**/*.browser.{test,spec}.{js,ts}'
+					],
 					exclude: ['src/lib/server/**']
 				}
 			},
@@ -40,8 +43,14 @@ export default defineConfig({
 				test: {
 					name: 'server',
 					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}', 'scripts/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					include: [
+						'src/**/*.{test,spec}.{js,ts}',
+						'scripts/**/*.{test,spec}.{js,ts}'
+					],
+					exclude: [
+						'src/**/*.svelte.{test,spec}.{js,ts}',
+						'src/**/*.browser.{test,spec}.{js,ts}'
+					]
 				}
 			}
 		]
